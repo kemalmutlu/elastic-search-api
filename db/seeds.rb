777e7fd4e55c79112254d220ac8d750f672f5451
Kemal Mutlu <mutlukemal1717@gmail.com>
2023-08-19 16:14:57 +0300

    fix Docker Compose configuration to connect to Elasticsearch
    Added the necessary environment variable "ELASTICSEARCH_URL" to the docker-compose.yml file, allowing the Rails application to connect to Elasticsearch properly within the Docker environment. This resolves the issue of being unable to establish a connection and enables seamless interaction between the application and Elasticsearch.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# TODO: Try this 1.000.000 data with activerecord-import
-1000.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph
-  )
+chunk_size = 500
+total_records = 100000
+
+if Product.count.zero?
+  (0..(total_records / chunk_size)).each do |chunk_index|
+    start_index = chunk_index * chunk_size
+    end_index = [(chunk_index + 1) * chunk_size - 1, total_records - 1].min
+
+    data = (start_index..end_index).map do |i|
+      { title: Faker::Commerce.product_name, description: Faker::Lorem.paragraph }
+    end
+
+    unless data.empty?
+      Product.upsert_all(data)
+    end
+
+    puts "Processed records #{start_index} to #{end_index}"
+  end
 end
